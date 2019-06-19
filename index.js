bce06254cd1f1bc1fee5a0749c2f641e498e240b
Tomáš Kubát <kubat.tomas@studenti.gymjil>
2019-06-19 09:09:32 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
---
+++ initial.sh
@@ -0,0 +1,17 @@
+
+commands=('npm install electron', 'npm install --save sequelize', 'npm install --save mysql2')
+
+if [ $# -eq 0 ]; then
+    echo "asd"
+else
+    rm -rf node_modules/
+    for item in ${commands[*]}
+    do
+        printf "   %s\n" $item
+    done
+fi
+
+#for item in ${commands[*]}
+#do
+#    eval $item
+#done
